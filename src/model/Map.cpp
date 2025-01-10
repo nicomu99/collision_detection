af23901771e475e718d0a2b7fa3cd749b96d6bf3
Nicolai Müller <nicolaimu1212@gmail.com>
2025-01-10 18:05:25 +0100

    Refactored map creation

--- src/model/Map.cpp
+++ src/model/Map.cpp
@@ -7,12 +7,38 @@
 
 Map::Map() : map(ROWS, std::vector<Tile>(COLS)) {
     constexpr auto size = static_cast<double>(TILE_SIZE);
+    const auto top_tile = Tile({static_cast<double>(Constants::SCREEN_WIDTH) / 2, static_cast<double>(TILE_SIZE) / 2},
+                               0, {0, 0}, 0, Constants::SCREEN_WIDTH, TILE_SIZE, WALL);
+    const auto bottom_tile = Tile({
+                                      static_cast<double>(Constants::SCREEN_WIDTH) / 2,
+                                      Constants::SCREEN_HEIGHT - static_cast<double>(TILE_SIZE) / 2
+                                  },
+                                  0, {0, 0}, 0, Constants::SCREEN_WIDTH, TILE_SIZE, WALL);
+    const auto left_tile = Tile({
+                                    static_cast<double>(TILE_SIZE) / 2,
+                                    static_cast<double>(Constants::SCREEN_HEIGHT) / 2
+                                },
+                                0, {0, 0}, 0, TILE_SIZE, Constants::SCREEN_HEIGHT, WALL);
+    const auto right_tile = Tile({
+                                     static_cast<double>(Constants::SCREEN_WIDTH) - static_cast<double>(TILE_SIZE) / 2,
+                                     static_cast<double>(Constants::SCREEN_HEIGHT) / 2
+                                 },
+                                 0, {0, 0}, 0, TILE_SIZE, Constants::SCREEN_HEIGHT, WALL);
     for (int row = 0; row < ROWS; ++row) {
         for (int col = 0; col < COLS; ++col) {
             double pos_x = (col * size) + size / 2.0;
             double pos_y = (row * size) + size / 2.0;
-            TileType type = (row == 0 || col == 0 || row == ROWS - 1 || col == COLS - 1) ? WALL : GRASS;
-            map[row][col] = Tile({pos_x, pos_y}, 0, {0, 0}, 0, TILE_SIZE, TILE_SIZE, type);
+            if (row == 0) {
+                map[row][col] = top_tile;
+            } else if (row == ROWS - 1) {
+                map[row][col] = bottom_tile;
+            } else if (col == 0) {
+                map[row][col] = left_tile;
+            } else if (col == COLS - 1) {
+                map[row][col] = right_tile;
+            } else {
+                map[row][col] = Tile({pos_x, pos_y}, 0, {0, 0}, 0, TILE_SIZE, TILE_SIZE, GRASS);
+            }
         }
     }
 }
@@ -41,8 +67,8 @@ const Tile& Map::getTile(int x, int y) const {
 }
 
 const Tile& Map::getTile(double x, double y) const {
-    int cast_x = (int)x;
-    int cast_y = (int)y;
+    int cast_x = (int) x;
+    int cast_y = (int) y;
 
     return getTile(cast_x, cast_y);
 }
